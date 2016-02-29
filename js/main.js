2c9f266dc85d780b8fdf51676526edccc11b3ec6
sandheather <heather.e.sanders22@gmail.com>
2016-02-29 18:59:11 -0500

    Prompts

--- js/main.js
+++ js/main.js
@@ -5,3 +5,9 @@ var story;
 
 alert('Let’s make a story together!');
 
+colour = prompt('A colour (orange, indigo, etc.)');
+creature = prompt('A creature (dragon, moth, etc.)');
+adjective = prompt('An adjective (beautiful, super, etc.)');
+verb = prompt('A past tense verb (ran, burped, etc.)');
+story = prompt('Which story would you like? a) Snacking b) Escaping c) Winning Enter the letter below:');
+
